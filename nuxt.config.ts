b303da2d82ce43b25263c66c17c3a9bbcbd42104
Ambidiestro <mantoti013@gmail.com>
2026-07-08 00:17:30 -0400

    feat: estructura limpia de belcar

---
+++ nuxt.config.ts
@@ -0,0 +1,18 @@
+export default defineNuxtConfig({
+  modules: ['@nuxt/eslint', '@nuxt/ui', '@pinia/nuxt'],
+
+  devtools: {
+    enabled: true
+  },
+
+  compatibilityDate: '2025-01-15',
+
+  eslint: {
+    config: {
+      stylistic: {
+        commaDangle: 'never',
+        braceStyle: '1tbs'
+      }
+    }
+  }
+})
